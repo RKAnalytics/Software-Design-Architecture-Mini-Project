c793a956990f33231edc849eac11829799d3e3ae
Rafay Khan <131152047+RKAnalytics@users.noreply.github.com>
2026-04-11 15:34:35 +0500

    Update the app.py and Wrote a code in deployment.yaml

--- app/app.py
+++ app/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>DevOps Project Running 🚀</h1>"
+    # return "<h1>DevOps Project Running 🚀</h1>"
+    return "<h1>Version 2 Running 🔥</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
